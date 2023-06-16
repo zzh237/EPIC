901237a451d09d63180cda285f215ba0715f74a5
zzh237 <zhizhang5763@gmail.com>
2023-06-16 12:45:10 -0700

    new

--- run_mc.sh
+++ run_mc.sh
@@ -2,13 +2,13 @@
 RUNS=1
 for n in 25
 do
-for step in 100
+for step in 300
 do
 for m in 100
 do
 for ((i=0;i<${RUNS};i++));
 do
-    python epic_mc.py --run ${i} --env "CartPole-v0" --meta_update_every $n --steps $step --mass 5 --m $m --goal 10.0 --resdir "results/montecarlo/new2/"
+    python epic_mc.py --run ${i} --env "CartPole-v0" --meta_update_every $n --steps $step --mass 5 --m $m --goal 10.0 --resdir "results/montecarlo/new1/"
 done
 done 
 done
