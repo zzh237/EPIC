b0b55ae8a0c7848e60d7830d5a8f8059660df7e0
zzh237 <zhizhang5763@gmail.com>
2023-06-17 23:14:54 -0700

    new

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash  
 RUNS=3
-for n in 25
+for n in 1
 do
 for step in 100 300
 do
@@ -8,7 +8,7 @@ for e in 10
 do
 for ((i=0;i<${RUNS};i++));
 do
-    python epic.py --run ${i} --env "CartPole-v0" --meta_update_every $n --meta-episodes $e --steps $step --mass 5 --goal 10.0 --resdir "results/test/single"
+    python epic.py --run ${i} --env "CartPole-v0" --meta_update_every $n --meta-episodes $e --steps $step --mass 5 --goal 10.0 --resdir "results/test/nosingle_kl"
 done 
 done
 done
