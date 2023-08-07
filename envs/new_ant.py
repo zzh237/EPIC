b97dcf3da24f51d45aa6a87fc0471a51f14197ef
HaochenZhang717 <haochen235@g.ucla.edu>
2023-08-06 21:00:37 -0700

    new test environments

--- envs/new_ant.py
+++ envs/new_ant.py
@@ -3,7 +3,7 @@ from gym import utils
 from gym.envs.mujoco import mujoco_env
 
 
-class new_AntEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+class AntDirection(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
         theta = np.random.uniform(0, 2*np.pi)
         self.goal_direction = np.array([np.sin(theta), np.cos(theta)])
@@ -56,3 +56,62 @@ class new_AntEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
     def viewer_setup(self):
         self.viewer.cam.distance = self.model.stat.extent * 0.5
+
+
+
+
+
+class AntForwardBackward(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        theta = np.random.choice([0, np.pi])
+        self.goal_direction = np.array([np.sin(theta), np.cos(theta)])
+        mujoco_env.MujocoEnv.__init__(self, "ant.xml", 5)
+        utils.EzPickle.__init__(self)
+
+    def step(self, a):
+        xposbefore = self.get_body_com("torso")[:2]
+        self.do_simulation(a, self.frame_skip)
+        xposafter = self.get_body_com("torso")[:2]
+        forward_reward = np.sum(self.goal_direction * (xposafter - xposbefore)) / self.dt
+        ctrl_cost = 0.5 * np.square(a).sum()
+        contact_cost = (
+            0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        )
+        survive_reward = 1.0
+        reward = forward_reward - ctrl_cost - contact_cost + survive_reward
+        state = self.state_vector()
+        notdone = np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
+        done = not notdone
+        ob = self._get_obs()
+        return (
+            ob,
+            reward,
+            done,
+            dict(
+                reward_forward=forward_reward,
+                reward_ctrl=-ctrl_cost,
+                reward_contact=-contact_cost,
+                reward_survive=survive_reward,
+            ),
+        )
+
+    def _get_obs(self):
+        return np.concatenate(
+            [
+                self.sim.data.qpos.flat[2:],
+                self.sim.data.qvel.flat,
+                np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
+            ]
+        )
+
+    def reset_model(self):
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq, low=-0.1, high=0.1
+        )
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * 0.1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def viewer_setup(self):
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
+
--- epic_mc.py
+++ epic_mc.py
@@ -12,14 +12,14 @@ import setup
 from algos.memory import Memory, ReplayMemory
 from algos.agents.gaussian_vpg_mc import GaussianVPGMC
 from algos.agents.gaussian_ppo import GaussianPPO
+
 from envs.new_cartpole import NewCartPoleEnv
 from envs.new_lunar_lander import NewLunarLander
-from envs.new_swimmer import new_Swimmer
-from envs.new_ant import new_AntEnv
+from envs.new_ant import AntDirection, AntForwardBackward
+from envs.new_halfcheetah import HalfCheetahForwardBackward
+from envs.new_humanoid import HumanoidDirection, HumanoidForwardBackward
 
-from envs.new_half_cheetah import new_HalfCheetahEnv
-# from envs.new_walker_2d import new_Walker2dEnv
-# from stable_baselines.common.env_checker import check_env
+
 
 import logging
 import copy
@@ -171,26 +171,40 @@ def make_car_env(env="MountainCarContinuous-v0"):
 
 def make_half_cheetah(env='half_cheetah'):
     assert env == 'half_cheetah', "env_name should be half_cheetah."
-    env = new_HalfCheetahEnv()
+    env = HalfCheetahForwardBackward()
     return env
 
-def make_swimmer(env='Swimmer'):
-    goal = np.random.uniform(low=-0.5, high=0.5)
-    env = new_Swimmer(goal=goal)
+def make_antdirection(env):
+    # assert env=='Ant'
+    env = AntDirection()
     return env
 
-def make_ant(env='Ant'):
-    assert env=='Ant'
-    env = new_AntEnv()
+def make_antforwardbackward(env):
+    # assert env=='Ant'
+    env = AntForwardBackward()
     return env
 
-def make_walker(env='walker_2d'):
-    assert env=='walker_2d'
-    env = new_Walker2dEnv()
+def make_humanoiddirection(env):
+    env = HumanoidDirection()
     return env
 
-envs = {'Swimmer':make_swimmer, 'LunarLander-v2': make_lunar_env, 'CartPole-v0':make_cart_env,
-        'half_cheetah': make_half_cheetah, 'Ant': make_ant, 'walker_2d': make_walker}
+def make_humanoidforwardbackward(env):
+    env = HumanoidForwardBackward()
+    return env
+
+# def make_walker(env='walker_2d'):
+#     assert env=='walker_2d'
+#     env = new_Walker2dEnv()
+#     return env
+
+envs = {'LunarLander-v2': make_lunar_env,
+        'CartPole-v0':make_cart_env,
+        'AntDirection': make_antdirection,
+        'AntForwardBackward': make_antforwardbackward,
+        'HalfcheetahForwardBackward': make_half_cheetah,
+        'HumanoidDirection': make_humanoiddirection,
+        'HumanoidForwardBackward': make_humanoidforwardbackward,
+        }
 
 if __name__ == '__main__':
     ############## Hyperparameters ##############
