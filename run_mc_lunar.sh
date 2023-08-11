a01af9168ff2d3c954d65e9986076a2857bd75d0
zzh237 <zhizhang5763@gmail.com>
2023-08-11 16:34:22 -0700

    new

--- run_mc_lunar.sh
+++ run_mc_lunar.sh
@@ -5,11 +5,16 @@
 #$ -m bea
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/home/z/zzh237/.mujoco/mujoco210/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+
+device="cpu"
+if ["$device" = "cuda"]; then
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+fi
+
 source ~/.bashrc
 
 RUNS=1
-device="cpu"
+
 for n in 25 
 do
 for step in 300
