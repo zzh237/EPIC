0a7e438716612feda63df15904c468c8c904b933
zzh237 <zhizhang5763@gmail.com>
2023-08-11 19:24:37 -0700

    new

--- run_mc_Swimmer.sh
+++ run_mc_Swimmer.sh
@@ -19,7 +19,7 @@ for m in 10
 do
 for ((i=0;i<${RUNS};i++));
 do
-    python epic_mc.py --run ${i} --env "Swimmer" --device "${device}" --meta_update_every $n --steps $step --mass 5 --m $m --goal 10.0 --resdir "results/montecarlo/step${step}_7/"
+    python epic_mc.py --run ${i} --env "Swimmer" --device "${device}" --meta_update_every $n --steps $step --mass 5 --m $m --goal 10.0 --resdir "results/montecarlo/step${step}_9/"
 done
 done 
 done
