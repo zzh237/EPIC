be965229e3a8d6fcaeddc9d15673b946cb5b5924
zzh237 <zhizhang5763@gmail.com>
2023-08-11 18:59:10 -0700

    new

--- run_mc_lunar.sh
+++ run_mc_lunar.sh
@@ -14,13 +14,13 @@ RUNS=1
 
 for n in 25 
 do
-for step in 300
+for step in 100
 do
-for m in 15 
+for m in 10 
 do
 for ((i=0;i<${RUNS};i++));
 do
-    python epic_mc.py --run ${i} --env "LunarLander-v2" --device "${device}" --meta_update_every $n --steps $step --mass 5 --m $m --goal 10.0 --resdir "results/montecarlo/step${step}_7/"
+    python epic_mc.py --run ${i} --env "LunarLander-v2" --device "${device}" --meta_update_every $n --steps $step --mass 5 --m $m --goal 10.0 --resdir "results/montecarlo/step${step}_9/"
 done
 done 
 done
