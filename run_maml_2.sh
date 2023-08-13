d1faa7b93c3c6c227717a0e4124a0c5c006dc760
HaochenZhang717 <haochen235@g.ucla.edu>
2023-08-13 10:55:21 -0700

    new results 08-13

---
+++ run_maml_2.sh
@@ -0,0 +1,18 @@
+#! /bin/bash  
+RUNS=13
+for env_name in "AntForwardBackward" "HalfcheetahForwardBackward" "HumanoidDirection" "HumanoidForwardBackward"
+do
+for n in 5 10 25 50
+do 
+for step in 1000
+do
+for mass in 1.0
+do
+for ((i=12;i<${RUNS};i++));
+do
+    python maml.py --run ${i} --env $env_name --samples 1000 --device "cuda:1" --meta_update_every $n --steps $step --mass $mass --goal 10.0 --resdir "results_maml/ant2/"
+done
+done
+done
+done
+done
---
+++ run_maml_4.sh
@@ -0,0 +1,18 @@
+#! /bin/bash  
+RUNS=3
+for env_name in "AntForwardBackward" "HalfcheetahForwardBackward" "HumanoidDirection" "HumanoidForwardBackward"
+do
+for n in 5 10 25 50
+do 
+for step in 1000
+do
+for mass in 1.0
+do
+for ((i=2;i<${RUNS};i++));
+do
+    python maml.py --run ${i} --env $env_name --samples 1000 --device "cuda:0" --meta_update_every $n --steps $step --mass $mass --goal 10.0 --resdir "results_maml/ant2/"
+done
+done
+done
+done
+done
