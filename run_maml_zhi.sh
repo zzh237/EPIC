1cdf8be7042cdc1bb1964b9c310fe2fb789fe08e
zzh237 <zhizhang5763@gmail.com>
2023-08-13 03:01:51 -0700

    new

--- run_maml_zhi.sh
+++ run_maml_zhi.sh
@@ -21,7 +21,7 @@ for mass in 1.0
 do
 for ((i=0;i<${RUNS};i++));
 do
-    python maml.py --run ${i} --env $env_name --device "${device}" --meta_update_every $n --steps $step --mass $mass --resdir "results_maml/"
+    python maml.py --run ${i} --env $env_name --device "${device}" --meta_update_every $n --steps $step --mass $mass --resdir "results_maml/${env_name}/"
 done
 done
 done
