8921f9045482f3acb1755503781c14a19cd09759
“zzh237” <zhizhang5763@gmail.com>
2024-09-27 14:20:10 -0700

    fix densities in swimmer, add swimmer sweeps

--- envs/swimmer_epic_2.py
+++ envs/swimmer_epic_2.py
@@ -6,36 +6,41 @@ from importlib.resources import files
 
 import gym.envs.mujoco.assets
 import numpy as np
+import wandb
 
 
-def modify_swimmer_xml(viscosity: float = 0.1, mass1: float = 0.1, mass2: float = 0.1, mass3: float = 0.1):
+def modify_swimmer_xml(viscosity: float = 0.1, density1: float = 0.1, density2: float = 0.1, density3: float = 0.1):
     """
     Modify the XML of the swimmer environment sth the viscosity is as specified, then return
     a string path to a tmpfile containing the modified xml.
     """
-    with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".xml") as tmpfile:
         with files(gym.envs.mujoco.assets).joinpath("swimmer.xml").open("r") as infile:
             tree = ET.parse(infile)
             tree.find(".//*[@viscosity]").attrib["viscosity"] = str(viscosity)
 
-            for el, mass in zip(tree.findall(".//geom[@type='capsule']"), [mass1, mass2, mass3]):
-                el.attrib["mass"] = str(mass)
+            for el, density in zip(tree.findall(".//geom[@type='capsule']"), [density1, density2, density3]):
+                el.attrib["density"] = str(density)
 
             tree.write(tmpfile)
 
     return tmpfile.name
 
+class ObservationFloatWrapper(gym.ObservationWrapper):
+    def observation(self, obs):
+        return obs.astype(np.float32, copy=False)
+
 
 def make_swimmer(seed: int):
     rng = np.random.default_rng(seed)
 
-    viscosity = rng.uniform(0.001, 0.2)
-    mass1 = rng.uniform(0.001, 0.3)
-    mass2 = rng.uniform(0.001, 0.3)
-    mass3 = rng.uniform(0.001, 0.3)
+    viscosity = rng.uniform(0.08, 0.15)
+    density1 = rng.uniform(100, 2000)
+    density2 = rng.uniform(100, 2000)
+    density3 = rng.uniform(100, 2000)
 
-    env = gym.make(
-        "Swimmer-v3", xml_file=modify_swimmer_xml(viscosity=viscosity, mass1=mass1, mass2=mass2, mass3=mass3)
-    )
+    modified_xml = modify_swimmer_xml(viscosity=viscosity, density1=density1, density2=density2, density3=density3)
+
+    env = ObservationFloatWrapper(gym.make("Swimmer-v3", xml_file=modified_xml))
 
     return env
