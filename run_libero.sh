aa0caec0efd8f2fe0fc1c896589fa6f6df7c513b
“zzh237” <zhizhang5763@gmail.com>
2024-10-18 18:03:49 -0700

    add runscript for libero main

---
+++ run_libero.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -cwd
+# error = Merged with joblog
+#$ -o joblogs/joblog.$JOB_ID
+#$ -j y
+## Edit the line below as needed:
+#$ -l h_rt=5:00:00,h_data=8G,cuda=1,gpu
+## Modify the parallel environment
+## and the number of cores as needed:
+#$ -pe shared 4
+
+source /u/local/Modules/default/init/modules.sh
+
+module load gcc/11.3.0
+module load cuda/12.3
+
+source activate_pixi.sh
+python ./scripts/libero_main.py
