466b35de9f8437227036fe92401d8034937f06db
“zzh237” <zhizhang5763@gmail.com>
2024-11-06 22:41:09 -0800

    use hydra override system

--- scripts/libero_main.py
+++ scripts/libero_main.py
@@ -20,97 +20,155 @@ from libero.libero import get_libero_path
 from libero.libero.benchmark import get_benchmark
 from libero.lifelong.datasets import GroupedTaskDataset, SequenceVLDataset, get_dataset
 from libero.lifelong.utils import get_task_embs, create_experiment_dir, safe_device
-from omegaconf import OmegaConf
+from omegaconf import DictConfig, OmegaConf
 from epic_libero.algorithm import EPICAlgorithm, MyLifelongAlgo
 import epic_libero.policy
 from libero.lifelong.models.bc_transformer_policy import BCTransformerPolicy
-from functools import reduce
+import hydra
+import imageio
 
-hydra.initialize_config_module("libero.configs")
-hydra_cfg = hydra.compose(config_name="config")
-yml_cfg = OmegaConf.to_yaml(hydra_cfg)
-cfg = EasyDict(yaml.safe_load(yml_cfg))
+from libero.libero.envs import OffScreenRenderEnv, DummyVectorEnv
+from libero.lifelong.metric import raw_obs_to_tensor_obs
 
-cfg.device = "cuda"
-cfg.folder = get_libero_path("datasets")
-cfg.bddl_folder = get_libero_path("bddl_files")
-cfg.init_states_folder = get_libero_path("init_states")
-cfg.eval.num_procs = 1
-cfg.eval.n_eval = 5
+# hydra.initialize_config_module("libero.configs")
+# hydra_cfg = hydra.compose(config_name="config")
+@hydra.main(config_name="user_config", config_path="../libero_config", version_base=None)
+def main(hydra_cfg: DictConfig):
+    yml_cfg = OmegaConf.to_yaml(hydra_cfg)
+    cfg = EasyDict(yaml.safe_load(yml_cfg))
 
-cfg.train.n_epochs = 25
+    cfg.folder = get_libero_path("datasets")
+    cfg.bddl_folder = get_libero_path("bddl_files")
+    cfg.init_states_folder = get_libero_path("init_states")
 
-task_order = cfg.data.task_order_index
-cfg.benchmark_name = "libero_10"
-benchmark = get_benchmark(cfg.benchmark_name)(task_order)
+    task_order = cfg.data.task_order_index
+    benchmark = get_benchmark(cfg.benchmark_name)(task_order)
 
-datasets = []
-descriptions = []
-shape_meta = None
-n_tasks = benchmark.n_tasks
+    datasets = []
+    descriptions = []
+    shape_meta = None
+    n_tasks = benchmark.n_tasks
 
-for i in range(n_tasks):
-    task_i_dataset, shape_meta = get_dataset(
-        dataset_path=os.path.join(cfg.folder, benchmark.get_task_demonstration(i)),
-        obs_modality=cfg.data.obs.modality,
-        initialize_obs_utils=(i == 0),
-        seq_len=cfg.data.seq_len,
+    for i in range(n_tasks):
+        task_i_dataset, shape_meta = get_dataset(
+            dataset_path=os.path.join(cfg.folder, benchmark.get_task_demonstration(i)),
+            obs_modality=cfg.data.obs.modality,
+            initialize_obs_utils=(i == 0),
+            seq_len=cfg.data.seq_len,
+        )
+        descriptions.append(benchmark.get_task(i).language)
+        datasets.append(task_i_dataset)
+
+    task_embs = get_task_embs(cfg, descriptions)
+    benchmark.set_task_embs(task_embs)
+
+    datasets = [SequenceVLDataset(ds, emb) for (ds, emb) in zip(datasets, task_embs)]
+    n_demos = [data.n_demos for data in datasets]
+    n_sequences = [data.total_num_sequences for data in datasets]
+
+
+    # TODO determine if it is safe to do this, specifically does the algorithm use the name
+    # to reinitialize the policy
+    # cfg.policy.policy_type = "EpicBayesianPolicy"
+    # cfg.lifelong.algo = "MyLifelongAlgo"
+
+    create_experiment_dir(cfg)
+    cfg.shape_meta = shape_meta
+
+    print(f"experiment directory is: {cfg.experiment_dir}")
+
+    algo = safe_device(MyLifelongAlgo(n_tasks, cfg), cfg.device)
+
+    result_summary = {
+        'L_conf_mat': np.zeros((n_tasks, n_tasks)),   # loss confusion matrix
+        'S_conf_mat': np.zeros((n_tasks, n_tasks)),   # success confusion matrix
+        'L_fwd'     : np.zeros((n_tasks,)),           # loss AUC, how fast the agent learns
+        'S_fwd'     : np.zeros((n_tasks,)),           # success AUC, how fast the agent succeeds
+    }
+
+    gsz = cfg.data.task_group_size
+
+    if (cfg.train.n_epochs < 50):
+        print(f"NOTE: the number of epochs used in this example is intentionally reduced to {cfg.train.n_epochs} for simplicity.")
+    if (cfg.eval.n_eval < 20):
+        print(f"NOTE: the number of evaluation episodes used in this example is intentionally reduced to {cfg.train.n_epochs} for simplicity.")
+
+    for i in trange(n_tasks):
+        algo.train()
+        d = datasets[i]
+        s_fwd, l_fwd = algo.learn_one_task(d, i, benchmark, result_summary)
+        # s_fwd is success rate AUC, when the agent learns the {0, e, 2e, ...} epochs
+        # l_fwd is BC loss AUC, similar to s_fwd
+        result_summary["S_fwd"][i] = s_fwd
+        result_summary["L_fwd"][i] = l_fwd
+
+        if cfg.eval.eval:
+            algo.eval()
+            # we only evaluate on the past tasks: 0 .. i
+            L = evaluate_loss(cfg, algo, benchmark, datasets[:i+1]) # (i+1,)
+            S = evaluate_success(cfg, algo, benchmark, list(range((i+1)*gsz))) # (i+1,)
+            result_summary["L_conf_mat"][i][:i+1] = L
+            result_summary["S_conf_mat"][i][:i+1] = S
+
+            torch.save(result_summary, os.path.join(cfg.experiment_dir, 'result.pt'))
+    
+    # single trial rollout on a new task
+    env_num = 1
+    task_id = n_tasks
+    task = benchmark.get_task(task_id)
+    task_emb = benchmark.get_task_emb(task_id)
+
+    algo.eval()
+    env_args = {
+        "bddl_file_name": os.path.join(cfg.bddl_folder, task.probelm_folder, task.bddl_file),
+        "camera_heights": cfg.data.img_h,
+        "camera_widths": cfg.data.img_w,
+    }
+
+    env = DummyVectorEnv(
+        [lambda: OffScreenRenderEnv(**env_args) for _ in range(env_num)]
     )
-    descriptions.append(benchmark.get_task(i).language)
-    datasets.append(task_i_dataset)
+    init_states_path = os.path.join(cfg.init_states_folder, task.problem_folder, task.init_states_file)
+    init_states = torch.load(init_states_path)
 
-task_embs = get_task_embs(cfg, descriptions)
-benchmark.set_task_embs(task_embs)
+    env.reset()
 
-datasets = [SequenceVLDataset(ds, emb) for (ds, emb) in zip(datasets, task_embs)]
-n_demos = [data.n_demos for data in datasets]
-n_sequences = [data.total_num_sequences for data in datasets]
+    init_state = init_states[0:1]
+    dones = [False]
 
+    algo.reset()
 
-# TODO determine if it is safe to do this, specifically does the algorithm use the name
-# to reinitialize the policy
-cfg.policy.policy_type = "EpicBayesianPolicy"
-cfg.lifelong.algo = "MyLifelongAlgo"
+    obs = env.set_init_state(init_state)
+    # assume that all tasks in the life have the same actiondim
+    action_dim = shape_meta["ac_dim"]
 
-create_experiment_dir(cfg)
-cfg.shape_meta = shape_meta
+    # manually open gripper to make it consistent with demonstration
+    dummy_actions = np.zeros((env_num, action_dim))
+    for _ in range(5):
+        obs, _, _, _ = env.step(dummy_actions)
 
-print(f"experiment directory is: {cfg.experiment_dir}")
+    steps = 0
+    obs_tensors = [[]] * env_num
 
-algo = safe_device(MyLifelongAlgo(n_tasks, cfg), cfg.device)
+    while steps < cfg.eval.max_steps:
+        steps += 1
+        data = raw_obs_to_tensor_obs(obs, task_emb, cfg)
+        action = algo.policy.get_action(data)
+        obs, reward, done, info = env.step(action)
 
-result_summary = {
-    'L_conf_mat': np.zeros((n_tasks, n_tasks)),   # loss confusion matrix
-    'S_conf_mat': np.zeros((n_tasks, n_tasks)),   # success confusion matrix
-    'L_fwd'     : np.zeros((n_tasks,)),           # loss AUC, how fast the agent learns
-    'S_fwd'     : np.zeros((n_tasks,)),           # success AUC, how fast the agent succeeds
-}
+        for k in range(env_num):
+            dones[k] = dones[k] or done[k]
+            obs_tensors[k].append(obs[k]["agentview_image"])
+        if all(dones):
+            break
 
-gsz = cfg.data.task_group_size
-
-if (cfg.train.n_epochs < 50):
-    print("NOTE: the number of epochs used in this example is intentionally reduced to 30 for simplicity.")
-if (cfg.eval.n_eval < 20):
-    print("NOTE: the number of evaluation episodes used in this example is intentionally reduced to 5 for simplicity.")
-
-# skip the first task, which has a weird shape?
-for i in trange(n_tasks):
-    algo.train()
-    d = datasets[i]
-    s_fwd, l_fwd = algo.learn_one_task(d, i, benchmark, result_summary)
-    # s_fwd is success rate AUC, when the agent learns the {0, e, 2e, ...} epochs
-    # l_fwd is BC loss AUC, similar to s_fwd
-    result_summary["S_fwd"][i] = s_fwd
-    result_summary["L_fwd"][i] = l_fwd
-
-    if cfg.eval.eval:
-        algo.eval()
-        # we only evaluate on the past tasks: 0 .. i
-        L = evaluate_loss(cfg, algo, benchmark, datasets[:i+1]) # (i+1,)
-        S = evaluate_success(cfg, algo, benchmark, list(range((i+1)*gsz))) # (i+1,)
-        result_summary["L_conf_mat"][i][:i+1] = L
-        result_summary["S_conf_mat"][i][:i+1] = S
-
-        torch.save(result_summary, os.path.join(cfg.experiment_dir, f'result.pt'))
+    images = [img[::-1] for img in obs_tensors[0]]
+    fps = 30
+    writer = imageio.get_writer(os.path.join(cfg.experiment_dir, "agentview.mp4"), fps=fps)
+    for image in images:
+        writer.append_data(image)
+    writer.close()
 
 
+if __name__ == "__main__":
+    main()
