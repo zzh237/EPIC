d07839ccebfcb524bcfbd748f27774f92523cbb0
“zzh237” <zhizhang5763@gmail.com>
2024-11-12 12:24:09 -0800

    add lockfile and experiment stuff for newest runs

--- run_libero.sh
+++ run_libero.sh
@@ -4,7 +4,7 @@
 #$ -o joblogs/joblog.$JOB_ID
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=20:00:00,h_data=8G,cuda=1,gpu,h_vmem=8G,RTX2080Ti
+#$ -l h_rt=20:00:00,h_data=8G,cuda=1,gpu,h_vmem=10G,RTX2080Ti
 ## Modify the parallel environment
 ## and the number of cores as needed:
 #$ -pe shared 4
